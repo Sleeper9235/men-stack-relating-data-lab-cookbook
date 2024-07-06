c5d1f28f83f8104476aab657e6240c220c158486
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-06 14:56:25 -0500

    add CRUD functionality

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -33,6 +33,55 @@ router.post('/', async (req, res) => {
     console.log(err)
     res.redirect('/')
   }
+})
 
+router.get("/:recipeId", async (req, res) => {
+  try {
+    const myRecipe = await Recipe.findById(req.params.recipeId).populate('owner')
+    res.render('recipes/show.ejs', {
+      recipe: myRecipe,
+    })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+router.delete("/:recipeId", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId).populate('owner')
+    if (recipe.owner.equals(req.session.user._id)) {
+      await recipe.deleteOne()
+      res.redirect('/recipes')
+    } else {
+      res.send("You don;t have permission to do that")
+    }
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+} )
+
+router.get("/:recipeId/edit", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId)
+    res.render('recipes/edit.ejs', {
+      recipe: recipe,
+    })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+router.put("/:recipeId", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId)
+    await recipe.updateOne(req.body)
+    res.redirect('/recipes')
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
 })
 module.exports = router;
